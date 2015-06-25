ffb4d354ee4187b01799734e72e5aadb370f04ce
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2015-06-25 11:35:11 -0700

    Follow SensorBee's breaking change

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,13 +1,12 @@
 package plugin
 
 import (
-	"pfi/sensorbee/sensorbee/bql"
 	"pfi/sensorbee/sensorbee/bql/udf"
 	"pfi/sensorbee/snowflake"
 )
 
 func init() {
-	if err := bql.RegisterGlobalUDSCreator("snowflake_id", bql.UDSCreatorFunc(snowflake.NewState)); err != nil {
+	if err := udf.RegisterGlobalUDSCreator("snowflake_id", udf.UDSCreatorFunc(snowflake.NewState)); err != nil {
 		panic(err)
 	}
 	if err := udf.RegisterGlobalUDF("snowflake_id", udf.UnaryFunc(snowflake.Snowflake)); err != nil {
